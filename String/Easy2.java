ce64420237e566698fcebcd616f1a7367d2508ab
patilsuhani860 <patilsuhani860@gmail.com>
2026-06-16 18:57:13 +0530

    length of string using java

---
+++ String/Easy2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count =0;
+        s=s.strip();
+        for(int i=s.length()-1; i>=0; i--)
+        {
+          if(s.charAt(i)!=' ')
+          {count++;}
+          else
+          break;
+        }
+        return count;
+    }
+}
